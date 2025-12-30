af9a13c34a99a62273377bcf8a37b172af379f98
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-12-30 18:24:20 +0530

    Initial Commit... auth-service structure created successfully..

---
+++ src/main/java/com/pharmassist/auth/PharmassistAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pharmassist.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmassistAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmassistAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharmassist/auth/PharmassistAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmassist.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmassistAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
